33196dbbe3e7a4fd5a3baf7f34c16c3c6b278ada
Sumedha <pramods@umich.edu>
2014-01-29 01:06:47 -0500

    trial 2?

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var app = express();
 app.use(logfmt.requestLogger());
 
 app.get('/', function(req, res) {
-  res.send('ThatBrown Groupme Bot');
+  res.send('ThatBrown Groupme Bot'); 
 });
 
 var port = Number(process.env.PORT || 5000);
