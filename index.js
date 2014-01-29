c7ec8feee636622a19b13bfc6298bfd1ff55cabc
Sumedha <pramods@umich.edu>
2014-01-29 12:22:51 -0500

    herpy derp

--- index.js
+++ index.js
@@ -260,11 +260,10 @@ incoming.on('message', function(msg) {
 
               request(url, function (error, response, body) {
                 if (!error && response.statusCode == 200) {
-                  console.log(body) // Print the google web page.
+                  console.dir(JSON.parse(response));
                 }
                 else {
-                  console.dir("Response: " + response);
-                  console.dir("Body: " + body);
+                  console.dir("Error: " + error);
                 }
               })
 
