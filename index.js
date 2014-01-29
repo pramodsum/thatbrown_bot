cc4bee45ef4ef0ae2f04c74e530b96cf92b61a2b
Sumedha <pramods@umich.edu>
2014-01-29 12:41:17 -0500

    removed search?

--- index.js
+++ index.js
@@ -257,7 +257,7 @@ incoming.on('message', function(msg) {
               var hmac = crypto.createHmac("sha256", private_key);
               var album_id = txt.replace("ThatBrown review ", "");
 
-              var url = "http://www.bollywoodapi.com/v1/search/albums/" + album_id + "/songs/?DeveloperID=" + dev_id + "&Version=1.0";
+              var url = "http://www.bollywoodapi.com/v1/albums/" + album_id + "/songs/?DeveloperID=" + dev_id + "&Version=1.0";
 
               request(url, function (error, response, body) {
                 if (!error && response.statusCode == 200) {
