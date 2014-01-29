c68cd70ed4006ed9c934f8e2ddf6840d67eb3998
Sumedha <pramods@umich.edu>
2014-01-29 02:11:47 -0500

    temporarily commented out google calendar stuff

--- index.js
+++ index.js
@@ -6,51 +6,51 @@ var app = express();
  * Getting the google calendar/passport Oauth configured and set up:
  ***********************************************************************/
 
-var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+// var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 
-// API Access link for creating client ID and secret:
-// https://code.google.com/apis/console/
-var GOOGLE_CLIENT_ID = "956631596569-o6vkaeqqtaq25vkm88h6a1m8mmd1ja6n.apps.googleusercontent.com";
-var GOOGLE_CLIENT_SECRET = "x3sB6IR_kSrGXEjeqKR_xH09";
-
-
-// Passport session setup.
-//   To support persistent login sessions, Passport needs to be able to
-//   serialize users into and deserialize users out of the session.  Typically,
-//   this will be as simple as storing the user ID when serializing, and finding
-//   the user by ID when deserializing.  However, since this example does not
-//   have a database of user records, the complete Google profile is
-//   serialized and deserialized.
-passport.serializeUser(function(user, done) {
-  done(null, user);
-});
-
-passport.deserializeUser(function(obj, done) {
-  done(null, obj);
-});
+// // API Access link for creating client ID and secret:
+// // https://code.google.com/apis/console/
+// var GOOGLE_CLIENT_ID = "956631596569-o6vkaeqqtaq25vkm88h6a1m8mmd1ja6n.apps.googleusercontent.com";
+// var GOOGLE_CLIENT_SECRET = "x3sB6IR_kSrGXEjeqKR_xH09";
 
 
-// Use the GoogleStrategy within Passport.
-//   Strategies in Passport require a `verify` function, which accept
-//   credentials (in this case, an accessToken, refreshToken, and Google
-//   profile), and invoke a callback with a user object.
-passport.use(new GoogleStrategy({
-    clientID: GOOGLE_CLIENT_ID,
-    clientSecret: GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://127.0.0.1:3000/auth/google/callback"
-  },
-  function(accessToken, refreshToken, profile, done) {
-    // asynchronous verification, for effect...
-    process.nextTick(function () {
+// // Passport session setup.
+// //   To support persistent login sessions, Passport needs to be able to
+// //   serialize users into and deserialize users out of the session.  Typically,
+// //   this will be as simple as storing the user ID when serializing, and finding
+// //   the user by ID when deserializing.  However, since this example does not
+// //   have a database of user records, the complete Google profile is
+// //   serialized and deserialized.
+// passport.serializeUser(function(user, done) {
+//   done(null, user);
+// });
+
+// passport.deserializeUser(function(obj, done) {
+//   done(null, obj);
+// });
+
+
+// // Use the GoogleStrategy within Passport.
+// //   Strategies in Passport require a `verify` function, which accept
+// //   credentials (in this case, an accessToken, refreshToken, and Google
+// //   profile), and invoke a callback with a user object.
+// passport.use(new GoogleStrategy({
+//     clientID: GOOGLE_CLIENT_ID,
+//     clientSecret: GOOGLE_CLIENT_SECRET,
+//     callbackURL: "http://127.0.0.1:3000/auth/google/callback"
+//   },
+//   function(accessToken, refreshToken, profile, done) {
+//     // asynchronous verification, for effect...
+//     process.nextTick(function () {
       
-      // To keep the example simple, the user's Google profile is returned to
-      // represent the logged-in user.  In a typical application, you would want
-      // to associate the Google account with a user record in your database,
-      // and return that user instead.
-      return done(null, profile);
-    });
-  }
-));
+//       // To keep the example simple, the user's Google profile is returned to
+//       // represent the logged-in user.  In a typical application, you would want
+//       // to associate the Google account with a user record in your database,
+//       // and return that user instead.
+//       return done(null, profile);
+//     });
+//   }
+// ));
 
 var app = express.createServer();
 
