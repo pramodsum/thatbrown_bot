6adee9ab9405e3da96ffee80ae47e6e07abbc683
Sumedha <pramods@umich.edu>
2014-01-30 12:51:20 -0500

    wrong time... bad parsing

--- index.js
+++ index.js
@@ -197,7 +197,7 @@ incoming.on('message', function(msg) {
 
             else if(txt.search("next bus") != -1) {
               var url = "http://mbus.pts.umich.edu/text/index.php?&route=Bursley-Baits#Bursley-Baits";
-              var looking_for = '<a name="Bursley-Baits%20C.C.%20Little%20SE" >C.C. Little</a>&nbsp;<a title="click to refresh stop C.C. Little SE for route Bursley-Baits" href="index.php?&route=Bursley-Baits&stop=C.C.%20Little%20SE#Bursley-Baits%20C.C.%20Little%20SE" >';
+              var looking_for = '<a title="click to refresh stop C.C. Little SE for route Bursley-Baits" href="index.php?&route=Bursley-Baits&stop=C.C.%20Little%20SE#Bursley-Baits%20C.C.%20Little%20SE" >[';
 
               request({
                 uri: url,
@@ -206,8 +206,9 @@ incoming.on('message', function(msg) {
 
                 var line_num = body.indexOf(looking_for);
                 if(line_num == -1) return;
-                
-                var time = body.substr(line_num + 2, 2);
+
+                var time = body.substr(line_num + 1, 2);
+                console.dir("TIME: " + time);
                 if(time.charAt(1) == 'm') time.replace(time.substr(0,1));
 
                 var message = "Next Bursley Baits bus @ CC Little in: " + time + " minutes\n";
